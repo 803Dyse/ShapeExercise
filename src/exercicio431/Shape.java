864193e61d10a6ae1bee9907c50b0d7799fe4e4b
bmargon <bmargon@DAW1-33.dip.local>
2023-01-18 12:21:47 +0100

    1st change

---
+++ src/exercicio431/Shape.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio431;
+
+/**
+ *
+ * @author Bilo Alejandro Martins Gonzalez
+ */
+public abstract class Shape {
+
+    public void draw() {
+    }
+
+    public void erase() {
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
